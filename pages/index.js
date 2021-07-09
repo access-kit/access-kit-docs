700671a7508eac21f73719438f58a7e51bb37dc2
Patrick Steadman <ptsteadman@gmail.com>
2021-07-09 04:01:37 -0400

    Fix botched merge
    Signed-off-by: Patrick Steadman <ptsteadman@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -137,20 +137,11 @@ export default function Index() {
                       <div className="text-blueGray-700 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
                         <i className="fas fa-file-alt"></i>
                       </div>
-<<<<<<< HEAD
                       <h6 className="text-xl mb-1 font-bold font-lede">
-                        Proven Case Studies
-                      </h6>
-                      <p className="mb-4 text-blueGray-700">
-                        See AccessKit in action at some of New York's premiere cultural institutions, like <a href="https://captioner.theshed.org/"><strong>the Shed</strong></a> and <a href="https://captioner.whitney.org"><strong>the Whitney Museum of American Art</strong></a>.
-
-=======
-                      <h6 className="text-xl mb-1 font-semibold">
                         Trusted
                       </h6>
                       <p className="mb-4 text-blueGray-500">
-                        See accesskit in action at some of New York's premiere cultural institutions, like <a href="https://captioner.theshed.org/"><strong>the Shed</strong></a> and <a href="https://captioner.whitney.org"><strong>the Whitney Museum of American Art</strong></a>.
->>>>>>> 5634edd41d472e9f10866c127e4309ca30ce0cdf
+                        See AccessKit in action at some of New York's premiere cultural institutions, like <a href="https://captioner.theshed.org/"><strong>the Shed</strong></a> and <a href="https://captioner.whitney.org"><strong>the Whitney Museum of American Art</strong></a>.
                       </p>
                     </div>
                   </div>
